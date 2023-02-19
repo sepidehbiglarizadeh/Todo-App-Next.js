09762d65e2a1c90a0e857d5fb34a0c6fd00db079
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-02-19 16:26:17 +0330

    Securing pages on the Server-Side

--- src/pages/protected-ssr.js
+++ src/pages/protected-ssr.js
@@ -4,13 +4,28 @@ const ProtectedSSR = () => {
   const { data: session, status } = useSession();
   console.log(session);
 
-  return <h1>Protected Page</h1>;
+  return (
+    <h1 className="text-center mt-6">
+      {session.user.name} Welcome to Protected Page
+    </h1>
+  );
 };
 
 export default ProtectedSSR;
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination:
+          "/api/auth/signin?callbackUrl=http://localhost:3000/protected-ssr",
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: {
       session,
