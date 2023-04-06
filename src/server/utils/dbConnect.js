b4f3abf0c32a66b222a1d303fb304c3a416a6908
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-04-06 15:12:28 +0330

    fix an error

--- src/server/utils/dbConnect.js
+++ src/server/utils/dbConnect.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export default function dbconnect(params) {
+  mongoose.set("strictQuery", false);
   mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected successfully..."))
